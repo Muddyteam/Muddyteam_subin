e7f90010498dab0395fed112604224c7f2c2f9a6
steamedEggMaster <cksdud5014@naver.com>
2024-07-31 11:00:47 +0900

    first commit

---
+++ src/main/java/com/example/muddyteam_subin/MuddyteamSubinApplication.java
@@ -0,0 +1,13 @@
+package com.example.muddyteam_subin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuddyteamSubinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuddyteamSubinApplication.class, args);
+	}
+
+}
